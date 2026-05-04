d44f5402c1fb8a4af159f407b6c9add20b2fa9ff
Vinícius Clemente Negherbon <vinicius.negherbon@neomind.com.br>
2026-05-04 19:35:52 -0300

    feat: versão atualizada do projeto

---
+++ src/main/java/com/monify/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.monify.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String password;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String name;
+}
